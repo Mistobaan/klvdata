4d06ac22212a65b851615a4ea1f12945f2d97a55
Fabrizio (Misto) Milo <mistobaan@gmail.com>
2019-08-16 11:45:25 -0700

    address case when the size is > maxint

--- klvdata/klvparser.py
+++ klvdata/klvparser.py
@@ -23,10 +23,14 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import sys
 from io import BytesIO
 from io import IOBase
-from klvdata.common import bytes_to_int
+from klvdata.common import bytes_to_int, ber_decode, hexstr_to_bytes
 
+LV_KEY_HEADER = hexstr_to_bytes('06 0E 2B 34')
+LV_KEY_V1 = hexstr_to_bytes('06 0E 2B 34 - 01 01 01 01 - 0F 00 00 00 - 00 00 00 00')
+LV_KEY_V2 = hexstr_to_bytes('06 0E 2B 34 - 02 0B 01 01 – 0E 01 03 01 - 01 00 00 00')
 
 class KLVParser(object):
     """Return key, value pairs parsed from an SMPTE ST 336 source."""
@@ -42,7 +46,7 @@ class KLVParser(object):
         return self
 
     def __next__(self):
-        key = self.__read(self.key_length)
+        key = self.__align_to_key()
 
         byte_length = bytes_to_int(self.__read(1))
 
@@ -57,11 +61,40 @@ class KLVParser(object):
 
         return key, value
 
+    def __align_to_key(self):
+        b = bytearray(self.key_length)
+        n = self.source.readinto(b)
+        if n == 0 or n < len(b):
+            raise StopIteration
+        while True:
+            idx = b.find(LV_KEY_HEADER)
+            if idx == 0:
+                return bytes(b)
+
+            if idx == -1:
+                # not found but could be across boundaries
+                # expand the buffer. keep the last 3 bytes of the buffer
+                len_keep_buffer = 4-1
+                b[-len_keep_buffer:] += self.__read(16 - 3)
+                continue
+            # if idx != 0 && idx != -1
+            # we need to read more data
+            available = n-idx
+            missing = 16 - available
+            bb = bytearray(missing)
+            nn = self.source.readinto(bb)
+            if nn != missing:
+                # Data stream terminated
+                raise StopIteration
+            key = b[idx:] + b[:nn]
+            return bytes(key)
+
     def __read(self, size):
         if size == 0:
             return b''
 
-        assert size > 0
+        assert size > 0, size
+        assert size < sys.maxsize, size
 
         data = self.source.read(size)
 
@@ -69,4 +102,3 @@ class KLVParser(object):
             return data
         else:
             raise StopIteration
-
